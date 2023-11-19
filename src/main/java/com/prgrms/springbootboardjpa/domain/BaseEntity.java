2ada5ef012facf3cc753888ea97f1a64171480a5
kimnamgyu <lethargic.hood@gmail.com>
2023-11-19 21:46:03 +0900

    feat : User, Post 도메인에 공통으로 사용되는 BaseEntity 구현

---
+++ src/main/java/com/prgrms/springbootboardjpa/domain/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.prgrms.springbootboardjpa.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime cratedAt;
+
+}
